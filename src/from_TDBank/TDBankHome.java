f920bea1be7a834347ef34dd688020fbfe84d73c
alelignqa <alent.qa@gmail.com>
2021-03-02 13:57:59 -0500

    adding TDBank methods and testing

--- src/from_TDBank/TDBankHome.java
+++ src/from_TDBank/TDBankHome.java
@@ -1,23 +1,59 @@
 package from_TDBank;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TDBankHome {
 
-    public static String url = "https://www.td.com/us/en/personal-banking/";
+    public static String url = "https://www.tdbank.com";
     public static WebDriver driver;
-    public static String driverLocation = "browsers_driver/Windows/chromedriver.exe";
+    public static String driverLocation = "browsers_driver/Windows/geckodriver.exe";
+    public static String browserDriver;
 
-    @Test
+    @BeforeMethod
     public static void setUp() throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver",driverLocation);
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver",driverLocation);
+        driver = new FirefoxDriver();
         driver.get(url);
         driver.manage().window().maximize();
-        Thread.sleep(3000);
+        Thread.sleep(5000);
     }
+
+    public static void getOfferButton() throws InterruptedException {
+
+        driver.findElement(By.xpath("//*[@id=\"ensCall\"]/span[2]")).click(); // to close the pop up window
+        driver.findElement(By.cssSelector("a.td-button:nth-child(3)")).click();
+    }
+
+    public static void smallBusiness() throws InterruptedException {
+
+        driver.findElement(By.xpath("//*[@id=\"ensCall\"]/span[2]")).click(); // to close the pop up window
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/header[1]/div[2]/div/div[1]/div/ul/li[2]/a")).click();
+    }
+
+    @Test
+    public static void commercial() throws InterruptedException {
+
+        driver.findElement(By.xpath("//*[@id=\"ensCall\"]/span[2]")).click(); // to close the pop up window
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/header[1]/div[2]/div/div[1]/div/ul/li[3]/a")).click();
+    }
+
+  @Test
+    public static void investingAndWealth() throws InterruptedException {
+
+        driver.findElement(By.xpath("//*[@id=\"ensCall\"]/span[2]")).click(); // to close the pop up window
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/header[1]/div[2]/div/div[1]/div/ul/li[4]/a")).click();
+    }
+
+
+
 
 }
--- src/from_TDBank/TestingTDBank.java
+++ src/from_TDBank/TestingTDBank.java
@@ -10,7 +10,40 @@ public class TestingTDBank extends TDBankHome{
         TDBankHome.setUp();
         String expectedTitlePage = "TD Personal Banking, Loans, Cards & More | TD Bank";
         String actualTitlePage = driver.getTitle();
-        Assert.assertEquals(actualTitlePage,expectedTitlePage,"Test failed");
-
+        Assert.assertEquals(actualTitlePage, expectedTitlePage, "Test failed");
     }
+
+    @Test
+    public void verifyOfferLink() throws InterruptedException {
+        TDBankHome.getOfferButton();
+        String expectedTitlePage = "Checking & Savings Account Offers";
+        String actualTitlePage = driver.getTitle();
+        Assert.assertEquals(actualTitlePage, expectedTitlePage, "Test failed");
+    }
+
+    @Test
+    public void verifySmallBusiness() throws InterruptedException {
+        TDBankHome.smallBusiness();
+        String expectedTitlePage = "Small Business Banking by TD Bank";
+        String actualTitlePage = driver.getTitle();
+        Assert.assertEquals(actualTitlePage, expectedTitlePage, "Test failed");
+    }
+
+    @Test
+    public void verifyCommercial() throws InterruptedException {
+        TDBankHome.commercial();
+        String expectedTitlePage = "TD Commercial Business Banking";
+        String actualTitlePage = driver.getTitle();
+        Assert.assertEquals(actualTitlePage, expectedTitlePage, "Test failed");
+    }
+
+ @Test
+    public void verifyInvestingWealth() throws InterruptedException {
+        TDBankHome.investingAndWealth();
+        String expectedTitlePage = "Wealth & Investment Management And Financial Planning | TD Wealth";
+        String actualTitlePage = driver.getTitle();
+        Assert.assertEquals(actualTitlePage, expectedTitlePage, "Test failed");
+    }
+
+
 }
