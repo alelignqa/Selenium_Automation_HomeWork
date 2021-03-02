dfa660721a130bdf409d30bc2905400418cfac36
alelignqa <alent.qa@gmail.com>
2021-03-02 01:25:43 -0500

    adding seven test cases from Cigna

--- src/health_care/from_cigna/CignaHome.java
+++ src/health_care/from_cigna/CignaHome.java
@@ -13,12 +13,13 @@ public class CignaHome {
     public static WebDriver driver;
     public static String driverPath = "browsers_driver/Windows/chromedriver.exe";
     public static String propertySet = "webdriver.chrome.driver";
-    public static String xpathSearch = "//*[@id=\"1564310141627\"]/div/header/div[2]/nav[1]/div/ul/li[4]/form/div/div/span/button";
+    public static String searchButton = "//*[@id=\"ssi-includes\"]/div/header/div[2]/nav[1]/div/ul/li[4]/form/div/div/span/button";
     public static String cssSearchBox = "#search-desktop";
     public static String classAttribute = "search-results__keyword";
     public static String searchResult = "//*[@id=\"cignaTab\"]/div[2]/div[1]/div/a";
 
-
+    //li:nth-child(4) > form > div > div > span > button
+    // //*[@id="ssi-includes"]/div/header/div[2]/nav[1]/div/ul/li[4]/form/div/div/span/button
 
     @BeforeMethod
     public static void  setUp(){
@@ -29,18 +30,36 @@ public class CignaHome {
     }
 
     public static void covidSearch() throws InterruptedException {
-        CignaHome.setUp();
+
         driver.findElement(By.cssSelector(cssSearchBox)).sendKeys("covid");
-        driver.findElement(By.xpath(xpathSearch)).click();
+        driver.findElement(By.xpath(searchButton)).click();
+        Thread.sleep(5000);
     }
-    @Test
+
     public static void readFirstSearchResult() throws InterruptedException {
         CignaHome.covidSearch();
-        Thread.sleep(5000);
         driver.findElement(By.xpath(searchResult)).click();
-
+        Thread.sleep(5000);
     }
-    @AfterMethod
+
+    public static void individualsAndFamilies(){
+        driver.findElement(By.cssSelector("#individuals-families-level-one-link")).click();
+    }
+
+    public static void madicar(){
+        driver.findElement(By.cssSelector("#medicare-level-one-link")).click();
+    }
+
+     public static void employersAndBrokers(){
+        driver.findElement(By.cssSelector("#employers-brokers-level-one-link")).click();
+    }
+    @Test
+   public static void healthCareProvider(){
+        driver.findElement(By.cssSelector("#health-care-providers-level-one-link")).click();
+    }
+
+
+  //  @AfterMethod
     public void tearDown(){
         driver.quit();
     }
---
+++ src/health_care/from_cigna/TestingCigna.java
@@ -0,0 +1,65 @@
+package health_care.from_cigna;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestingCigna extends  CignaHome{
+
+    @Test
+    public void verifyHomePage(){
+
+        String expectedTitle = "Cigna Official Site | Global Health Service Company";
+        String actualTile = driver.getTitle();
+        Assert.assertEquals(actualTile,expectedTitle,"Test failed");
+    }
+
+    @Test
+    public void verifyCovidSearch() throws InterruptedException {
+        CignaHome.covidSearch();
+        String expectedText = "Displaying results for covid";
+        String actuealText = driver.findElement(By.className("search-results__keyword")).getText();
+        Assert.assertEquals(actuealText,expectedText,"Test faild");
+    }
+
+    @Test
+    public void readSearchResult() throws InterruptedException {
+        CignaHome.readFirstSearchResult();
+        String expectedTitle = "Coronavirus (COVID-19) Information for Medicare and Medicaid | Cigna";
+        String actualTile = driver.getTitle();
+        Assert.assertEquals(actualTile,expectedTitle,"Test failed");
+    }
+    @Test
+    public void verifyIndividualsAndFamiliesPage(){
+        CignaHome.individualsAndFamilies();
+        String expectedTitle = "Individual and Family Health Insurance Plans | Cigna";
+        String actualTile = driver.getTitle();
+        Assert.assertEquals(actualTile,expectedTitle,"Test failed");
+    }
+
+    @Test
+    public void verifyMedicarPage(){
+        CignaHome.madicar();
+        String expectedTitle = "Cigna Medicare Insurance | Cigna";
+        String actualTile = driver.getTitle();
+        Assert.assertEquals(actualTile,expectedTitle,"Test failed");
+    }
+
+ @Test
+    public void verifyEmplorAndBrokers(){
+        CignaHome.employersAndBrokers();
+        String expectedTitle = "Employer Health Insurance Plans | Cigna";
+        String actualTile = driver.getTitle();
+        Assert.assertEquals(actualTile,expectedTitle,"Test failed");
+    }
+
+@Test
+    public void verifyhealthCareProvider(){
+        CignaHome.healthCareProvider();
+        String expectedTitle = "Health Care Providers | Cigna";
+        String actualTile = driver.getTitle();
+        Assert.assertEquals(actualTile,expectedTitle,"Test failed");
+    }
+
+
+}
